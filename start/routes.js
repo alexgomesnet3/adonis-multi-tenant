89333a6a219089d618be66de47345d410d26152c
Alex Gomes <alex.gomes.net3@gmail.com>
2020-05-10 22:35:06 -0300

    Creating Permission Controller and routes show permissions that belongs to User auth

--- start/routes.js
+++ start/routes.js
@@ -25,4 +25,6 @@ Route.group(() => {
 
   Route.get('members', 'MemberController.index')
   Route.put('members/:id', 'MemberController.update').middleware('is:administrator')
+
+  Route.get('permissions', 'PermissionController.show')
 }).middleware(['auth', 'team'])
